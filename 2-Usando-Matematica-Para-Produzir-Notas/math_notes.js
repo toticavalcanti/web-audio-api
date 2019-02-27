13b471c7d7555d6b6c8465b656229f2842130e1f
Toti Cavalcanti <toticavalcanti@hotmail.com>
2019-02-27 13:03:10 -0300

    fix some simple things

--- 2-Usando-Matematica-Para-Produzir-Notas/math_notes.js
+++ 2-Usando-Matematica-Para-Produzir-Notas/math_notes.js
@@ -1,17 +1,16 @@
 $( function() {
 
-  // configura o contexto de áudio
-  var context = new AudioContext();
-  window.context = context;
+  // pega um contexto de áudio
+  var ctx = new AudioContext();
 
   // variável que vai representar o oscilador
-  var oscillator;
+  var osc;
 
   // números que precisaremos depois
   var width   = $(window).width();
   var height  = $(window).height();
-  var xValue  = 0;
-  var yValue  = 0;
+  var x = 0;
+  var y = 0;
 
   // recalcule a largura e a altura se o tamanho da janela mudar
   $(window).resize( function() {
@@ -20,44 +19,44 @@ $( function() {
   });
 
   $('body').on('mousedown', function(e) {
-    if (oscillator) {
-      oscillator.stop(0);
+    if (osc) {
+      osc.stop(0);
     }
     // cria o oscilador
-    oscillator = context.createOscillator();
+    osc = ctx.createOscillator();
     // define o tipo do oscilador
-    oscillator.type = 'triangle'; // sine, triangle, sawtooth
+    osc.type = 'triangle'; // sine, triangle, sawtooth
 
     // a função mtof recebe uma nota midi como argumento e retorna a freqüência correspondente em Hertz.
-    // a função mtof define a frequência com base nos valores xValue e yValue
-    oscillator.frequency.value = mtof(xValue + yValue);
+    // a função mtof define a frequência com base nos valores x e y
+    osc.frequency.value = mtof(x + y);
     // conecta-o à saída, isto é, o destino
-    oscillator.connect(context.destination);
+    osc.connect(ctx.destination);
     // inicia a nota
-    oscillator.start(0);
+    osc.start(0);
   });
 
   $('body').on('mouseup', function(e) {
     // para a nota quando desclica o botão do mouse (mouseup)
-    oscillator.stop(0);
+    osc.stop(0);
   });
 
   $('body').on('mousemove', function(e) {
     // faz algumas contas para colocar os valores em intervalos de números:
     // converte (0 <-> largura da janela) para (0 <-> 13) e arredonda a parte decimal pare baixo
-    xValue = 45 + Math.floor( e.clientX / width * 13 );
+    x = 45 + Math.floor( e.clientX / width * 13 );
     // converte (0 <-> altura da janela) para (0 <-> 4) e arredonda a parte decimal pare baixo
-    yValue = Math.floor( e.clientY / height * 4 ) * 12;
+    y = Math.floor( e.clientY / height * 4 ) * 12;
 
-    if (!oscillator) {
+    if (!osc) {
       return;
     }
 
     // versão menos precisa
-    //oscillator.frequency.value = mtof(xValue + yValue);
+    //osc.frequency.value = mtof(x + y);
 
     // versão usando setValueAtTime que é mais preciso em relação ao tempo
-    oscillator.frequency.setValueAtTime( mtof(xValue + yValue), context.currentTime );
+    osc.frequency.setValueAtTime( mtof(x + y), ctx.currentTime );
   });
 
 });
