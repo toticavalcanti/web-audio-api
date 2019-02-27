0bd47d888d976dc155b29a8da3c8a5df7f01c1bc
Toti Cavalcanti <toticavalcanti@hotmail.com>
2019-02-27 15:57:17 -0300

    insert x and y coordinates in html and js

--- 2-Usando-Matematica-Para-Produzir-Notas/math_notes.js
+++ 2-Usando-Matematica-Para-Produzir-Notas/math_notes.js
@@ -6,6 +6,10 @@ $( function() {
   // variável que vai representar o oscilador
   var osc;
 
+  // obtém os elementos span x e y para que possamos exibir valores deles
+  var horizontal = $('#x');
+  var vertical   = $('#y');
+
   // pega a largura e a altura
   var width   = $(window).width();
   var height  = $(window).height();
@@ -25,7 +29,7 @@ $( function() {
     // cria o oscilador
     osc = ctx.createOscillator();
     // define o tipo do oscilador
-    osc.type = 'triangle'; // sine, triangle, sawtooth
+    osc.type = 'sawtooth'; // sine, triangle, sawtooth
 
     // a função mtof recebe uma nota midi como argumento e retorna a freqüência correspondente em Hertz.
     // a função mtof define a frequência com base nos valores x e y
@@ -42,12 +46,19 @@ $( function() {
   });
 
   $('body').on('mousemove', function(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+
+    //Mostra as coordenadas na tela
+    horizontal.text("X = " + x);
+    vertical.text("Y = " + y);
     // faz algumas contas para colocar os valores em intervalos de números:
     // converte (0 <-> largura da janela) para (0 <-> 13) e arredonda a parte decimal pare baixo
     x = 45 + Math.floor( e.clientX / width * 13 );
+
     // converte (0 <-> altura da janela) para (0 <-> 4) e arredonda a parte decimal pare baixo
     y = Math.floor( e.clientY / height * 4 ) * 12;
-
+    
     if (!osc) {
       return;
     }
